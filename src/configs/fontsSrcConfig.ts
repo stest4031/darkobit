f13860fee071dfff377eb6ab0227169859f030ff
ramin_kp <ramin.kp81@gmail.com>
2025-07-23 14:45:15 +0330

    initialize project

---
+++ src/configs/fontsSrcConfig.ts
@@ -0,0 +1,59 @@
+const danaSrc = [
+  {
+    path: "../../public/fonts/DanaFaNum-Thin.woff2",
+    weight: "100",
+    style: "normal",
+  },
+  {
+    path: "../../public/fonts/DanaFaNum-UltraLight.woff2",
+    weight: "200",
+    style: "normal",
+  },
+  {
+    path: "../../public/fonts/DanaFaNum-Light.woff2",
+    weight: "300",
+    style: "normal",
+  },
+  {
+    path: "../../public/fonts/DanaFaNum-Regular.woff2",
+    weight: "400",
+    style: "normal",
+  },
+  {
+    path: "../../public/fonts/DanaFaNum-Medium.woff2",
+    weight: "500",
+    style: "normal",
+  },
+  {
+    path: "../../public/fonts/DanaFaNum-DemiBold.woff2",
+    weight: "600",
+    style: "normal",
+  },
+  {
+    path: "../../public/fonts/DanaFaNum-Bold.woff2",
+    weight: "700",
+    style: "normal",
+  },
+  {
+    path: "../../public/fonts/DanaFaNum-ExtraBold.woff2",
+    weight: "800",
+    style: "normal",
+  },
+  {
+    path: "../../public/fonts/DanaFaNum-Heavy.woff2",
+    weight: "800",
+    style: "normal",
+  },
+  {
+    path: "../../public/fonts/DanaFaNum-ExtraBlack.woff2",
+    weight: "900",
+    style: "normal",
+  },
+  {
+    path: "../../public/fonts/DanaFaNum-Black.woff2",
+    weight: "900",
+    style: "normal",
+  },
+];
+
+export { danaSrc };
